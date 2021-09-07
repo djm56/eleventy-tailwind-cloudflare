0409c0ea4b1df68c448765412c42d7d28b2007df
Donovan Maidens <donovan@anomalous.co.za>
2021-09-07 15:28:52 +0200

    Adding Logo

--- .eleventy.js
+++ .eleventy.js
@@ -3,29 +3,35 @@ const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
 
 module.exports = function(eleventyConfig) {
 
-  eleventyConfig.addPlugin(eleventyNavigationPlugin);
+    eleventyConfig.addPlugin(eleventyNavigationPlugin);
+    eleventyConfig.setTemplateFormats([
+        "svg",
+        "njk",
+        "md"
+        //"js",
+        //"css"
+    ]);
+    eleventyConfig.addPassthroughCopy('src/images')
+    eleventyConfig.addPassthroughCopy('admin')
 
-  eleventyConfig.addPassthroughCopy('images')
-  eleventyConfig.addPassthroughCopy('admin')
+    const {
+        DateTime
+    } = require("luxon");
 
-  const {
-    DateTime
-  } = require("luxon");
-
-  // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
+    // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
     eleventyConfig.addFilter('htmlDateString', (dateObj) => {
-      return DateTime.fromJSDate(dateObj, {
-        zone: 'utc'
-      }).toFormat('yy-MM-dd');
+        return DateTime.fromJSDate(dateObj, {
+            zone: 'utc'
+        }).toFormat('yy-MM-dd');
     });
 
     eleventyConfig.addFilter("readableDate", dateObj => {
-    return DateTime.fromJSDate(dateObj, {
-      zone: 'utc'
-    }).toFormat("dd-MM-yy");
-  });
+        return DateTime.fromJSDate(dateObj, {
+            zone: 'utc'
+        }).toFormat("dd-MM-yy");
+    });
 
-  return {
-    dir: { input: 'src', output: '_site' }
-  };
+    return {
+        dir: { input: 'src', output: '_site' }
+    };
 };
--- src/blog/posts/my-2nd-post.md
+++ src/blog/posts/my-2nd-post.md
@@ -4,4 +4,4 @@ title: Anomalous second post
 description: The second post on the blog
 date: 2020-09-01
 ---
-Hello, here is the body of the post.
+Hello, here is the body of the post. test
